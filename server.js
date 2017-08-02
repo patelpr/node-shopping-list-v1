ff00b9d46c495f920af4f989c0032df378bded1d
patelpr <parth.patel.r@gmail.com>
2017-08-02 05:41:31 -0400

    recipes redo

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const { RecipeList } = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -12,18 +11,17 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
+// we're going to add some items to RecipeList
 // so there's some data to look at
-ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
-ShoppingList.create('peppers', 4);
+RecipeList.create('chocolate milk', ['cocoa', 'milk', 'sugar'])
+
 
 // when the root of this router is called with GET, return
-// all current ShoppingList items
-app.get('/shopping-list', (req, res) => {
-  res.json(ShoppingList.get());
+// all current RecipeList items
+app.get('/recipes', (req, res) => {
+    res.json(RecipeList.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
